1fb330a2eed6debd2622fe5d59fe8268cd5a6f2a
Lucas <lucasfm801@gmail.com>
2025-10-18 18:18:46 -0300

    teste de API

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,24 +1,43 @@
-describe('Teste de adicionar item ao carrinho', () => {
-  it('Adicionado item  com sucesso', () => {
-    cy.visit('https://automationexercise.com/')
+describe("Teste de adicionar item ao carrinho", () => {
+  it("Adicionado item  com sucesso", () => {
+    cy.visit("https://automationexercise.com/");
     cy.get('a[href="/product_details/1"]').click();
-    cy.get('button.cart').click();
-    cy.get('#cartModal h4.modal-title').contains('Added');
-    cy.get('#cartModal u').click();
-    cy.get('#product-1 a[href="/product_details/1"]').contains('Blue Top');
-    
-  })
-})
+    cy.get("button.cart").click();
+    cy.get("#cartModal h4.modal-title").contains("Added");
+    cy.get("#cartModal u").click();
+    cy.get('#product-1 a[href="/product_details/1"]').contains("Blue Top");
+  });
+});
 
-describe('Teste de remover item do carrinho', () => {
-  it('Item removido  com sucesso', () => {
-    cy.visit('https://automationexercise.com/')
+describe("Teste de remover item do carrinho", () => {
+  it("Item removido  com sucesso", () => {
+    cy.visit("https://automationexercise.com/");
     cy.get('a[href="/product_details/1"]').click();
-    cy.get('button.cart').click();
-    cy.get('#cartModal h4.modal-title').contains('Added');
-    cy.get('#cartModal u').click();
-    cy.get('#product-1 a[href="/product_details/1"]').contains('Blue Top');
-    cy.get('#product-1 i.fa').click();
-    
-  })
-})
+    cy.get("button.cart").click();
+    cy.get("#cartModal h4.modal-title").contains("Added");
+    cy.get("#cartModal u").click();
+    cy.get('#product-1 a[href="/product_details/1"]').contains("Blue Top");
+    cy.get("#product-1 i.fa").click();
+  });
+});
+
+describe("Teste de consulta de produtos via API", () => {
+  it("Get de todos os produtos", () => {
+    cy.request({
+      method: "GET",
+      url: "https://automationexercise.com/api/productsList",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+   it("Consulta um produto", () => {
+    cy.request({
+      method: "POST",
+      url: "https://automationexercise.com/api/productsList",
+      body:{search_product:'top'}
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+});
